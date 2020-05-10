c293c67664ea2a7fe3337efef5510996efc495bb
Tatiana Lebedeva <tea-cup-mail@yandex.ru>
2020-05-11 02:35:04 +0300

    feat: добавить функцию удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+/* Переменные для работы с карточками */
 const initialCards = [
   {
       name: 'Архыз',
@@ -27,12 +28,14 @@ const initialCards = [
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsContainer = document.querySelector('.photo-grid');
 
+/* Переменные для кнопок */
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 const likeButton = document.querySelector('.card__like-button');
 const closeEditFormButton = document.querySelector('#edit-form_close-button');
 const closeAddFormButton = document.querySelector('#add-form_close-button');
 
+/* Переменные для элементов Pop-Up */
 const popupElement = document.querySelectorAll('.popup');
 const popupEdit = document.querySelector('#edit-form');
 const popupAdd = document.querySelector('#add-form');
@@ -42,11 +45,6 @@ const inputProfileAbout = document.querySelector('.popup__input_type_user-about'
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 
-/*
-function openPopup () {
-  popupElement.classList.add('popup_open');
-}
-*/
 
 function closePopup () {
   popupEdit.classList.remove('popup_open');
@@ -70,18 +68,32 @@ function openAddCardPopup () {
   popupAdd.classList.add('popup_open');
 }
 
-function render() {
+function renderCards() {
   cardsContainer.innerHTML = '';
-  initialCards.forEach(renderItem);
+  initialCards.forEach(renderCard);
+  addEventListeners();
 }
 
-render();
+renderCards();
 
-function renderItem(item) {
+function renderCard(item, index) {
   const cardElement = cardTemplate.cloneNode(true);
   cardElement.querySelector('.card__name').innerText = item.name;
   cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card').setAttribute('id', index);
   cardsContainer.appendChild(cardElement);
+}
+
+function handleDelete(event) {
+  const index = event.target.parentNode.getAttribute('id');
+  initialCards.splice(index, 1);
+  renderCards();
+}
+
+function addEventListeners() {
+  document.querySelectorAll('.card__trash-button').forEach(deleteButton => {
+    deleteButton.addEventListener('click', handleDelete);
+  })
 }
 
 editButton.addEventListener('click', openProfilePopup);
